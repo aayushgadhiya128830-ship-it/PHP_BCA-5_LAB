5f702531a8001a1564aa4e8082f5660e2693d7c7
Aayush Gadhiya <aayush.gadhiya128830@marwadiuniversity.ac.in>
2026-06-29 12:47:29 +0530

    Add files via upload
    select

---
+++ Unit-2/select.php
@@ -0,0 +1,8 @@
+<html>
+	<body>
+		<form action="upload.php" method="POST" enctype="multipart/form-data">
+		Select File: <input type="file" name="fileToUpload"/>
+		<input type="submit" value="Upload Image" name="submit"/>
+		</form>
+	</body>
+</html>
